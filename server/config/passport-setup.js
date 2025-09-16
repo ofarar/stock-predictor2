3e3c9f3d57fa6105572f44ce16047c1566431c84
Omer Arar <ofarar@gmail.com>
2025-09-16 15:05:41 +0200

    Login is not working because of callback, fixed

--- server/config/passport-setup.js
+++ server/config/passport-setup.js
@@ -16,23 +16,18 @@ passport.use(
     new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: '/auth/google/callback'
+        callbackURL: process.env.GOOGLE_CALLBACK_URL
     }, async (accessToken, refreshToken, profile, done) => {
-        // Check if user already exists
         const existingUser = await User.findOne({ googleId: profile.id });
 
         if (existingUser) {
-            // Already have this user
             done(null, existingUser);
         } else {
-            // If not, create a new user AND their starting portfolio
             const newUser = await new User({
                 googleId: profile.id,
                 username: profile.displayName,
-                // You can add more fields like profile picture from profile.photos[0].value
             }).save();
-        
-            
+
             done(null, newUser);
         }
     })
