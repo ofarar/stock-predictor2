f6fcb6f0df98780468e1c6dcf6a65e77ab2d22bf
Omer Arar <ofarar@gmail.com>
2025-09-17 02:08:49 +0200

    Removed Edit profile sub menu

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -52,11 +52,6 @@ const Header = ({ user, onMakePredictionClick }) => {
                         <svg className="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path></svg>
                         My Profile
                     </Link>
-                    {/* Corrected "Edit Profile" Icon */}
-                    <Link to="/profile/edit" className="flex items-center px-4 py-2 text-sm text-gray-300 hover:bg-gray-700">
-                        <svg className="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path></svg>
-                        Edit Profile
-                    </Link>
                     {/* --- New Admin Link --- */}
                     {user.isAdmin && (
                         <Link to="/admin" className="flex items-center px-4 py-2 text-sm text-yellow-400 hover:bg-gray-700">
