0bbe8426dbe28ca9918de2935d7bc4409876f3c8
Omer Arar <ofarar@gmail.com>
2025-10-18 00:46:03 +0200

    Make footer links more responsive, cleaner

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -10,26 +10,29 @@ const Footer = () => {
     return (
         <footer className="bg-gray-900 border-t border-gray-800 mt-12 md:mt-20">
             <div className="container mx-auto py-8 px-4 sm:px-6 lg:px-8">
-                <div className="grid grid-cols-1 md:grid-cols-3 gap-8 items-center">
-                    {/* Logo on the left */}
-                    <div className="flex justify-center md:justify-start">
+                {/* Use flex for more natural layout control */}
+                <div className="flex flex-col md:flex-row items-center justify-between gap-6 flex-wrap md:flex-nowrap">
+                    
+                    {/* Left: Logo */}
+                    <div className="flex-shrink-0">
                         <Logo />
                     </div>
-                    
-                    {/* Links in the center */}
-                    <div className="flex flex-wrap justify-center gap-x-6 gap-y-2 text-gray-400">
+
+                    {/* Center: Links */}
+                    <div className="flex flex-wrap md:flex-nowrap justify-center gap-x-6 text-gray-400 whitespace-nowrap overflow-x-auto">
                         <Link to="/about" className="hover:text-white">{t('footer.about')}</Link>
                         <Link to="/contact" className="hover:text-white">{t('footer.contact')}</Link>
                         <Link to="/terms" className="hover:text-white">{t('footer.terms')}</Link>
                         <Link to="/privacy" className="hover:text-white">{t('footer.privacy')}</Link>
                     </div>
 
-                    {/* Language selector on the right */}
-                    <div className="flex justify-center md:justify-end">
-                        
-                    </div>
+                    {/* Right: Placeholder (language selector) */}
+                    <div className="flex justify-center md:justify-end flex-shrink-0"></div>
                 </div>
-                <div className="text-center text-gray-500 mt-8 pt-8 border-t border-gray-800">&copy; {currentYear} StockPredictor. {t('footer.copyright')}</div>
+
+                <div className="text-center text-gray-500 mt-8 pt-8 border-t border-gray-800">
+                    &copy; {currentYear} StockPredictor. {t('footer.copyright')}
+                </div>
             </div>
         </footer>
     );
