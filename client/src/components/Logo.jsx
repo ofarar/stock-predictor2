cb60a85c42a102ceae89dfe625a83b466c79aa50
Omer Arar <ofarar@gmail.com>
2026-08-08 09:16:56 +0200

    Removed beta tag

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -8,7 +8,7 @@ const Logo = () => {
             {/* Logo Image */}
             <img src="/logo.png" alt="StockPredictorAI Logo" className="w-8 h-8 object-contain" />
 
-            {/* This div handles the responsive text and BETA tag */}
+            {/* This div handles the responsive text */}
             <div className="flex flex-row items-center sm:flex-col sm:items-start">
 
                 {/* Logo Text: Hidden on mobile, visible on desktop */}
@@ -16,11 +16,6 @@ const Logo = () => {
                     Stock<span className="text-green-400">Predictor</span>
                     {/* --- NEW "AI" TEXT --- */}
                     <span className="text-xl text-gray-300 ms-1">AI</span>
-                </span>
-
-                {/* Beta Tag (no change) */}
-                <span className="bg-yellow-400 text-black text-xs font-bold px-2 py-0.5 rounded-md sm:w-fit sm:-mt-1">
-                    BETA
                 </span>
             </div>
 
